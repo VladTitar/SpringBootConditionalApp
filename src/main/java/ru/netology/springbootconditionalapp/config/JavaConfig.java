0bdadf1e521c3f296bf1fdc99b2d84e92281a16c
Vladislav Titar <titar-vlad@mail.ru>
2023-08-29 22:25:15 +0300

    Initial commit

---
+++ src/main/java/ru/netology/springbootconditionalapp/config/JavaConfig.java
@@ -0,0 +1,23 @@
+package ru.netology.springbootconditionalapp.config;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import ru.netology.springbootconditionalapp.profile.DevProfile;
+import ru.netology.springbootconditionalapp.profile.ProductionProfile;
+import ru.netology.springbootconditionalapp.profile.SystemProfile;
+
+@Configuration
+public class JavaConfig {
+    @Bean
+    @ConditionalOnProperty(name = "netology.profile.dev", havingValue = "true")
+    public SystemProfile devProfile() {
+        return new DevProfile();
+    }
+
+    @Bean
+    @ConditionalOnProperty(name = "netology.profile.dev", havingValue = "false")
+    public SystemProfile prodProfile() {
+        return new ProductionProfile();
+    }
+}
